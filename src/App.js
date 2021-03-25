8c5c9ca274f38a796b1039631cb258440628d652
Ethan Butt <ethanbutt309@gmail.com>
2021-03-25 14:38:29 -0500

    refactor: Update to match  current React and JS standards with functional components, useState, etc.

--- src/App.js
+++ src/App.js
@@ -1,122 +1,109 @@
-import React, { Component } from "react";
-import * as $ from "jquery";
-import { authEndpoint, clientId, redirectUri, scopes } from "./config";
-import hash from "./hash";
-import Player from "./Player";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useState, useEffect } from 'react';
+import * as $ from 'jquery';
+import { authEndpoint, clientId, redirectUri, scopes } from './config';
+import hash from './hash';
+import Player from './Player';
+import logo from './logo.svg';
+import './App.css';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      token: null,
-      item: {
-        album: {
-          images: [{ url: "" }]
-        },
-        name: "",
-        artists: [{ name: "" }],
-        duration_ms: 0
-      },
-      is_playing: "Paused",
-      progress_ms: 0,
-      no_data: false,
-    };
+const App = () => {
+  const [token, setToken] = useState(null);
+  const [item, setItem] = useState({
+    album: {
+      images: [{ url: '' }]
+    },
+    name: '',
+    artists: [{ name: '' }],
+    duration_ms: 0
+  });
+  const [isPlaying, setIsPlaying] = useState('Paused');
+  const [progressInMS, setProgressInMS] = useState(0);
+  const [noData, setNoData] = useState(false);
 
-    this.getCurrentlyPlaying = this.getCurrentlyPlaying.bind(this);
-    this.tick = this.tick.bind(this);
-  }
+  // getCurrentlyPlaying = getCurrentlyPlaying.bind(this);
+  // tick = tick.bind(this);
 
 
-
-  componentDidMount() {
+  // componentDidMount
+  useEffect(() => {
     // Set token
     let _token = hash.access_token;
 
     if (_token) {
       // Set token
-      this.setState({
-        token: _token
-      });
-      this.getCurrentlyPlaying(_token);
+      setToken(_token);
+      getCurrentlyPlaying(_token);
     }
 
     // set interval for polling every 5 seconds
-    this.interval = setInterval(() => this.tick(), 5000);
-  }
+    // interval = setInterval(() => tick(), 5000);
+  });
 
-  componentWillUnmount() {
-    // clear the interval to save resources
-    clearInterval(this.interval);
-  }
+  // componentWillUnmount
+  // const unload = () => {
+  //   // clear the interval to save resources
+  //   clearInterval(interval);
+  // }
 
-  tick() {
-    if(this.state.token) {
-      this.getCurrentlyPlaying(this.state.token);
-    }
-  }
+  // const tick = () => {
+  //   if(token) {
+  //     getCurrentlyPlaying(token);
+  //   }
+  // }
 
 
-  getCurrentlyPlaying(token) {
+  const getCurrentlyPlaying = (authToken) => {
     // Make a call using the token
     $.ajax({
-      url: "https://api.spotify.com/v1/me/player",
-      type: "GET",
+      url: 'https://api.spotify.com/v1/me/player',
+      type: 'GET',
       beforeSend: xhr => {
-        xhr.setRequestHeader("Authorization", "Bearer " + token);
+        xhr.setRequestHeader('Authorization', 'Bearer ' + authToken);
       },
       success: data => {
         // Checks if the data is not empty
         if(!data) {
-          this.setState({
-            no_data: true,
-          });
+          setNoData(true);
           return;
         }
 
-        this.setState({
-          item: data.item,
-          is_playing: data.is_playing,
-          progress_ms: data.progress_ms,
-          no_data: false /* We need to "reset" the boolean, in case the
-                            user does not give F5 and has opened his Spotify. */
-        });
+        setItem(data.item);
+        setIsPlaying(data.is_playing);
+        setProgressInMS(data.progress_ms);
+        setNoData(false); // We need to 'reset' the boolean, in case the user does not give F5 and has opened his Spotify.
       }
     });
   }
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          {!this.state.token && (
-            <a
-              className="btn btn--loginApp-link"
-              href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
-                "%20"
-              )}&response_type=token&show_dialog=true`}
-            >
-              Login to Spotify
-            </a>
-          )}
-          {this.state.token && !this.state.no_data && (
-            <Player
-              item={this.state.item}
-              is_playing={this.state.is_playing}
-              progress_ms={this.state.progress_ms}
-            />
-          )}
-          {this.state.no_data && (
-            <p>
-              You need to be playing a song on Spotify, for something to appear here.
-            </p>
-          )}
-        </header>
-      </div>
-    );
-  }
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        {!token && (
+          <a
+            className='btn btn--loginApp-link'
+            href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+              '%20'
+            )}&response_type=token&show_dialog=true`}
+          >
+            Login to Spotify
+          </a>
+        )}
+        {token && !noData && (
+          <Player
+            item={item}
+            isPlaying={isPlaying}
+            progressInMS={progressInMS}
+          />
+        )}
+        {noData && (
+          <p>
+            You need to be playing a song on Spotify, for something to appear here.
+          </p>
+        )}
+      </header>
+    </div>
+  );
 }
 
 export default App;
