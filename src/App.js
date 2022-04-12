0186f60e40abc7c0eb3d9364ec21dcb1566056c8
Ethan Bütt <ethanbutt309@gmail.com>
2022-04-11 20:55:32 -0500

    wip: Spotify cleanup.

--- src/App.js
+++ src/App.js
@@ -3,55 +3,95 @@ import * as $ from 'jquery';
 import { authEndpoint, clientId, clientSecret, redirectUri, scopes } from './config';
 import logo from './logo.svg';
 import './App.css';
+import { generateCode } from './rand';
+import sjcl from 'sjcl';
 
 const App = () => {
   const [token, setToken] = useState(null);
   const [playlists, setPlaylists] = useState([]);
 
   useEffect(() => {
-    let _token = localStorage.getItem('ACCESS_TOKEN');
-
-    if (!_token) {
       const params = (new URL(document.location)).searchParams;
-      let _code = params.get('code');
+      const _code = params.get('code');
+      const rState = generateCode(16);
+      const rCode = generateCode(127);
+      const challengeCodeBitArray = sjcl.hash.sha256.hash(rCode);
+      console.log('Random code: ', rCode);
+      const challengeCode = (new Buffer(sjcl.codec.hex.fromBits(challengeCodeBitArray)).toString('base64'));
       if (_code) {
         $.ajax({
-          url: `https://accounts.spotify.com/api/token?grant_type=authorization_code&code=${_code}&redirect_uri=${redirectUri}`,
+          url: `https://accounts.spotify.com/api/authorize?client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}&code_challenge_method=S256&state=${rState}&code_challenge=${challengeCode}`,
           type: 'POST',
-          beforeSend: xhr => {
-            xhr.setRequestHeader('Authorization', 'Basic ' + (new Buffer(clientId + ':' + clientSecret).toString('base64')));
+          beforeSend: (xhr) => {
             xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
           },
-          success: data => {
-            localStorage.setItem('REFRESH_TOKEN', data.refresh_token);
-            localStorage.setItem('ACCESS_TOKEN', data.access_token);
-            setToken(data.access_token);
-            getUserPlaylists(_token);
-
+          success: (data) => {
+            console.log('-- RES', data);
           },
           error: (xhr) => {
-            console.log(`${xhr.status}: ${xhr.statusText}`);
-            setToken(null);
+            console.log(`-- ${xhr.status}: ${xhr.statusText}`);
           },
         });
       }
-    } else {
-      setToken(_token);
-      getUserPlaylists(_token);
-    }
-  },
+    },
   []
   );
+
+  // useEffect(() => {
+  //   let _token = localStorage.getItem('ACCESS_TOKEN');
+
+  //   if (!_token) {
+  //     const params = (new URL(document.location)).searchParams;
+  //     const _code = params.get('code');
+  //     const rState = generateCode(16);
+  //     const rCode = generateCode(127);
+  //     const challengeCodeBitArray = sjcl.hash.sha256.hash(rCode);
+  //     console.log('Random code: ', rCode);
+  //     const challengeCode = (new Buffer(sjcl.codec.hex.fromBits(challengeCodeBitArray)).toString('base64'));
+  //     if (_code) {
+  //       $.ajax({ // &grant_type=authorization_code &state=${rState}
+  //         url: `https://accounts.spotify.com/api/token?
+  //         code=${_code}&
+  //         grant_type=authorization_code&
+  //         redirect_uri=${redirectUri}&
+  //         code_challenge_method=S256&
+  //         code_challenge=${challengeCode}`,
+  //         type: 'POST',
+  //         beforeSend: (xhr) => {
+  //           xhr.setRequestHeader('Authorization', 'Basic ' + (new Buffer(clientId + ':' + clientSecret).toString('base64')));
+  //           xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  //         },
+  //         success: (data) => {
+  //           console.log('RES', data);
+  //           const { access_token, refresh_token } = data;
+  //           localStorage.setItem('REFRESH_TOKEN', refresh_token);
+  //           localStorage.setItem('ACCESS_TOKEN', access_token);
+  //           setToken(access_token);
+  //           getUserPlaylists(access_token);
+  //         },
+  //         error: (xhr) => {
+  //           console.log(xhr.status === 400 ? 'Not logged in with Spotify' : `${xhr.status}: ${xhr.statusText}`);
+  //           setToken(null);
+  //         },
+  //       });
+  //     }
+  //   } else {
+  //     setToken(_token);
+  //     getUserPlaylists(_token);
+  //   }
+  // },
+  // []
+  // );
 
   const getUserPlaylists = (authToken) => {
     // Make a call using the token
     $.ajax({
       url: `https://api.spotify.com/v1/me/playlists`,
       type: 'GET',
-      beforeSend: xhr => {
+      beforeSend: (xhr) => {
         xhr.setRequestHeader('Authorization', 'Bearer ' + authToken);
       },
-      success: data => {
+      success: (data) => {
         console.log(data.items);
         setPlaylists(data.items);
       },
@@ -73,7 +113,7 @@ const App = () => {
               '%20'
             )}&response_type=code&show_dialog=true`}
           >
-            Login to Spotify
+            Log In to Spotify
           </a>
         )}
         {token && (
